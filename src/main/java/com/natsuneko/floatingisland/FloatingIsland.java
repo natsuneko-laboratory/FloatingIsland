e391c2d3810d8e8a94832804b0ec1beb1e0578c3
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-09-01 21:48:58 +0900

    refactor: cleanup old sources

--- src/main/java/com/natsuneko/floatingisland/FloatingIsland.java
+++ src/main/java/com/natsuneko/floatingisland/FloatingIsland.java
@@ -21,8 +21,6 @@ public class FloatingIsland implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 		Registry.register(Registry.CHUNK_GENERATOR, new Identifier(MOD_ID, "floating_island"), FloatingIslandChunkGenerator.CODEC);
 	}
 }
